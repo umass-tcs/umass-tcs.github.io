a0af22af610e224b5e9622ce3e54bd50d67778f0
augustlovesyou <psingh@umass.edu>
2026-08-03 21:57:23 +0000

    Deploying to gh-pages from @ umass-tcs/umass-tcs.github.io@b1f5dc9c405878e483e5d1ad7018df5ed44cbeaf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "nav-eboard",
-          title: "Eboard",
-          description: "Our Officers",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/eboard/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
